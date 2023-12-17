e7810c1ad03e1b56a8b5e55d2557a822417ce6e6
6gh <47262177+6gh@users.noreply.github.com>
2023-12-17 04:36:40 -0500

    midi list i forgot to commit

---
+++ assets/midis.ts
@@ -0,0 +1,516 @@
+export const MIDIs: MIDI[] = [
+  {
+    name: "1k Special",
+    date: "Oct 26, 2020",
+    description:
+      "hello guys! this is the 1k special midi i was working on and this version is the latest version I've made. i personally feel that even though it has a couple or more weaknesses, this is probably one of, if not, the best solo, original midis that ive made. ive spent like a month on it before I decided maybe its not good enough for 1k. but looking back on it I think I did a nice job and I don't think it was as bad as I thought while making this. tell me what you guys think! again, thank you all soooooo much for 1.1k subs, I am extremely grateful that you all decided to subscribe to me.  thank you ❤️",
+    id: "1k",
+    missing: false,
+    badges: [
+      {
+        text: "Special",
+        hoverText: "Thanks for 1k Subscribers!",
+        color: "error",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "6,803,182",
+        size: "51.9 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "TRR",
+    date: "Mar 29, 2020",
+    description:
+      "On Nov 4th, 2018, I planned to create The Set Of Songs, another medley (primarily to replace CoBM, another medley that didn't turn out well). I uploaded three versions of TSOS and people started helping out. TSOS went through a lot of changes until it became TRR (TSOS Remastered Revamped) and it turned into an awesome medley. Almost 1 year and 4 months later, TRR was finished and here it is. Thanks LucasMIDI, Grimmy, JM, Andy, and ZBT so much for helping me out on this medley. (TSOS, TRR, and TSOS Remastered are all the same medley, just different versions)",
+    id: "trr",
+    missing: false,
+    badges: [
+      {
+        text: "Collab",
+        hoverText: "This is a collaboration project",
+        color: "primary",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "TRR Team",
+        authors_team: "6gh, LucasMIDI, Grimmy, JM, Andy",
+        notes: "20,581,276",
+        size: "157.1 MB",
+        download_link: "#download-link",
+      },
+      u21: {
+        authors: "TRR Team",
+        authors_team: "6gh, LucasMIDI, Grimmy, JM, Andy",
+        notes: "2,251,409",
+        size: "17.2 MB",
+        download_link: "#download-link",
+      },
+      u20: {
+        authors: "TRR Team",
+        authors_team: "6gh, LucasMIDI, Grimmy, JM, Andy",
+        notes: "2,536,946",
+        size: "19.4 MB",
+        download_link: "#download-link",
+      },
+      u12: {
+        authors: "TRR Team",
+        authors_team: "6gh, LucasMIDI, Grimmy, JM, Andy",
+        notes: "979,004",
+        size: "7.5 MB",
+        download_link: "#download-link",
+      },
+      u2: {
+        authors: "TRR Team",
+        authors_team: "6gh, LucasMIDI, Grimmy, JM, Andy",
+        notes: "29,580",
+        size: "238.3 KB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "The Battle",
+    date: "Mar 8, 2019",
+    description:
+      "This solo composition was scrapped due to the fact that it was too repetitive and boring. I didn't know how to fix this so it ended up left behind in the work in progress stage.",
+    id: "the-battle",
+    missing: false,
+    versions: {
+      u4: {
+        authors: "6gh",
+        notes: "242,237",
+        size: "1.9 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "An Apple!!",
+    date: "Feb 15, 2019",
+    description:
+      'This MIDI is a part of my series named "IDCMIDIs", where the main objective was to create as big of MIDIs as possible. Hence, this MIDI is 99% spam and 1% audio.',
+    id: "an-apple",
+    missing: false,
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "60,545,084",
+        size: "461.9 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Super Mario Lag Tester",
+    date: "Jan 30, 2019",
+    description:
+      "Not sure why I decided to make this but I did. This is a MIDI that tests the lag of your computer. It's not really a MIDI, it's more of a test. (Black MIDI Benchmarking??? :scream:)",
+    id: "smlt",
+    missing: false,
+    versions: {
+      F2: {
+        authors: "6gh",
+        notes: "15,608,322",
+        size: "119.1 MB",
+        download_link: "#download-link",
+      },
+      F1: {
+        authors: "6gh",
+        notes: "5,557,319",
+        size: "42.4 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Mary had 6 lambs",
+    date: "Jan 28, 2019",
+    description:
+      'This MIDI has 2 finals as this MIDI was a collab with JMv5. The first final was "my version", since I liked the way F1 was, and the second final was "JMv5\'s version" where more of his edits are shown.',
+    id: "mh6l",
+    missing: false,
+    badges: [
+      {
+        text: "Collab",
+        hoverText: "This is a collaboration project",
+        color: "primary",
+      },
+    ],
+    versions: {
+      f2: {
+        authors: "6gh, JMv5",
+        notes: "3,487,463",
+        size: "26.60 MB",
+        download_link: "#download-link",
+      },
+      f1: {
+        authors: "6gh, JMv5",
+        notes: "3,286,325",
+        size: "25.09 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "The Dark Night",
+    date: "Oct 26, 2020",
+    description:
+      "Another solo composition that was scrapped due to the fact that it was too repetitive. I didn't know how to fix this so it ended up left behind in the work in progress stage.",
+    id: "tdn",
+    missing: false,
+    badges: [
+      {
+        text: "Collab",
+        hoverText: "This is a collaboration project",
+        color: "primary",
+      },
+    ],
+    versions: {
+      u4: {
+        authors: "6gh",
+        notes: "737,069",
+        size: "5.6 MB",
+        download_link: "#download-link",
+      },
+      u3: {
+        authors: "6gh",
+        notes: "711,093",
+        size: "5.4 MB",
+        download_link: "#download-link",
+      },
+      u2: {
+        authors: "6gh",
+        notes: "336,485",
+        size: "2.6 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Bad Apple.mid",
+    date: "Oct 26, 2020",
+    description:
+      "I remember the reason I started making this MIDI was because another Bad Apple MIDI had came out recently by someone else (I forgot who). After I saw it I decided to make a Bad Apple MIDI myself and it ended up being almost the same as the other one. I decided to release it anyway and now here we are",
+    id: "ba-mid",
+    missing: false,
+    badges: [
+      {
+        text: "Collab",
+        hoverText: "This is a collaboration project",
+        color: "primary",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "43,523",
+        size: "292.3 KB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "I'm backkk",
+    date: "Oct 26, 2020",
+    description:
+      "I had taken a long break from making MIDIs since I used to be a mobile blacker at this time, and my phone was broken. When I got my phone back I decided to make a MIDI to celebrate my return. Terrible MIDI. Don't download.",
+    id: "imback",
+    missing: false,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "6,803,182",
+        size: "51.91 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Bad Apple Basic Mode",
+    date: "Oct 26, 2020",
+    description:
+      "A Bad Apple MIDI that was meant to be a basic mode version of the original Bad Apple MIDI. By basic I meant something that can be run on a potato/phone without lagging. Yeah that didn't work out. This MIDI became a collab with HERNAN BLACKMIDIS and Aiyui, and it suddenly became a not-so-basic mode MIDI. It still has a precious place in my heart though. Thanks to HERNAN BLACKMIDIS and vowel man for helping me with this MIDI",
+    id: "babm",
+    missing: false,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+      {
+        text: "Collab",
+        hoverText: "This MIDI is a collaboration project",
+        color: "primary",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh, HERNAN BLACKMIDIS, Aiyui",
+        notes: "",
+        size: "",
+        download_link: "#download-link",
+      },
+      u6: {
+        authors: "6gh, HERNAN BLACKMIDIS",
+        notes: "1,587,531",
+        size: "12.1 MB",
+        download_link: "#download-link",
+      },
+      u5: {
+        authors: "6gh, HERNAN BLACKMIDIS",
+        notes: "155,125",
+        size: "1.2 MB",
+        download_link: "#download-link",
+      },
+      u4: {
+        authors: "6gh, HERNAN BLACKMIDIS",
+        notes: "105,107",
+        size: "798.5 KB",
+        download_link: "#download-link",
+      },
+      u3: {
+        authors: "6gh",
+        notes: "65,318",
+        size: "483 KB",
+        download_link: "#download-link",
+      },
+      u2: {
+        authors: "6gh",
+        notes: "38,813",
+        size: "275.7 KB",
+        download_link: "#download-link",
+      },
+      u1: {
+        authors: "6gh",
+        notes: "6,274",
+        size: "47 KB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Bad Apple Rainbow Mode",
+    date: "Oct 26, 2020",
+    description:
+      'The original idea of this was to make a Bad Apple "Skittle Mode" where the arts were in different tracks to change their colors. Yeah it was supposed to have a lot of colors, just like how Skittles are colorful. Can you tell I was obsessed with Bad Apple at the time? With all the Bad Apple MIDIs I was making? Yeah I had a LOT more but most were lost to time forever.',
+    id: "barm",
+    missing: false,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+      {
+        text: "Special",
+        hoverText: "Thanks for 30 subscribers!",
+        color: "error",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "1,231,801",
+        size: "9.1 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "CoBM",
+    date: "Oct 26, 2020",
+    description:
+      "My first ever attempt to make a medley. Main goal of this was undefined, there was no goal for this medley. It was just to compile MIDIs I really liked. So a lot of, um, copying took place. Sorry for that. I was heavily inexperienced at the time, but these MIDIs were public so I don't think it matters that much.",
+    id: "cobm",
+    missing: false,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+    ],
+    versions: {
+      V2: {
+        authors: "6gh",
+        notes: "1,057,922",
+        size: "8.0 MB",
+        download_link: "#download-link",
+      },
+      V1U2: {
+        authors: "6gh",
+        notes: "924,668",
+        size: "6.6 MB",
+        download_link: "#download-link",
+      },
+      V1U1: {
+        authors: "6gh",
+        notes: "678,332",
+        size: "4.9 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Bad Apple Chill Mode",
+    date: "Oct 26, 2020",
+    description:
+      "I don't know why I made this to be honest. I was just obsessed with Bad Apple at this time so this was born at the time I was working on Basic Mode.",
+    id: "bacm",
+    missing: false,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+      {
+        text: "Collab",
+        hoverText: "This MIDI is a collaboration project",
+        color: "primary",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "2,521,147",
+        size: "19.2 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "I'm bored.",
+    date: "Oct 26, 2020",
+    description: "I was bored. So I made a MIDI. End of story",
+    id: "bored",
+    missing: true,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+      {
+        text: "Missing",
+        hoverText: "This MIDI is missing! If you have it, please contact me!",
+        color: "outline",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "6,803,182",
+        size: "51.91 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "10 Subscriber Special",
+    date: "Oct 26, 2020",
+    description:
+      "I remember hitting 10 subscribers and feeling the need to celebrate them. I also remember constantly removing notes, exporting, playing, and then adding more notes just to get that sweet exact note count. That was funny.",
+    id: "10sub",
+    missing: true,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+      {
+        text: "Special",
+        hoverText: "Thanks for 10 subscribers!",
+        color: "error",
+      },
+      {
+        text: "Missing",
+        hoverText: "This MIDI is missing! If you have it, please contact me!",
+        color: "outline",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "6,803,182",
+        size: "51.91 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+  {
+    name: "Reckless",
+    date: "Oct 26, 2020",
+    description:
+      "Dear god, this was my first (technically second) MIDI. I decided to just straight up copy TSMB2's MIDI lmfao. I was so proud of this. I was so proud of myself. I *was*. I'm not anymore. Don't judge me",
+    id: "reckless",
+    missing: true,
+    badges: [
+      {
+        text: "Old",
+        hoverText: "This MIDI is old! Expect very poor quality",
+        color: "warning",
+      },
+      {
+        text: "Special",
+        hoverText: "My first still-public MIDI. Here's to 4 years of blacking!",
+        color: "error",
+      },
+      {
+        text: "Missing",
+        hoverText: "This MIDI is missing! If you have it, please contact me!",
+        color: "outline",
+      },
+    ],
+    versions: {
+      f1: {
+        authors: "6gh",
+        notes: "6,803,182",
+        size: "51.91 MB",
+        download_link: "#download-link",
+      },
+    },
+  },
+];
+
+export interface MIDI {
+  name: string;
+  date: string;
+  id: string;
+  description: string;
+  versions: {
+    [key: string]: Version;
+  };
+  missing: boolean;
+  badges?: Badge[];
+}
+
+export interface Version {
+  authors: string;
+  notes: string;
+  size: string;
+  download_link: string;
+
+  authors_team?: string;
+}
+
+export interface Badge {
+  color: `${"primary" | "warning" | "error" | "outline"}`;
+  text: string;
+  hoverText: string;
+}
