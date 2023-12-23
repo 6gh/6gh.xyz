422e16c5bd95cdbd407a4e82734594459fd9293f
6gh <47262177+6gh@users.noreply.github.com>
2023-12-23 00:58:34 -0500

    custom error page

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -2,9 +2,24 @@
 defineProps({
   image: {
     type: String,
+    required: true,
+  },
+  title: {
+    type: String,
     required: false,
-  }
-})
+    default: "6gh.xyz",
+  },
+  subtitle: {
+    type: String,
+    required: false,
+    default: "Welcome to 6gh's Main Website! Here you can find downloads to my MIDIs, contact information, and more!",
+  },
+  defaultButtons: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+});
 </script>
 
 <template>
@@ -12,14 +27,14 @@ defineProps({
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="py-64">
-        <h1 class="mb-5 text-5xl font-bold">6gh.xyz</h1>
-        <p class="mb-5 text-xl">Welcome to 6gh's Main Website! Here you can find downloads to my MIDIs, contact
-          information, and more!</p>
-        <div class="join">
+        <h1 class="mb-5 text-5xl font-bold">{{ title }}</h1>
+        <p v-if="subtitle.length > 1" class="mb-5 text-xl">{{ subtitle }}</p>
+        <div v-if="defaultButtons" class="join">
           <NuxtLink to="/midis" class="btn btn-primary join-item">MIDIs</NuxtLink>
           <NuxtLink to="/contact" class="btn btn-secondary join-item">Contact
           </NuxtLink>
         </div>
+        <slot />
       </div>
     </div>
   </div>
---
+++ error.vue
@@ -0,0 +1,30 @@
+<script setup>
+const error = useError();
+const image404 = useCdn("/images/404.png");
+const imageGeneric = useCdn("/images/error.png");
+</script>
+
+<template>
+    <NuxtLayout>
+        <template v-if="error.statusCode === 404">
+            <HeroBanner :image="image404" title="404"
+                subtitle="That page was not found! It either has been deleted, has been renamed, or never existed!"
+                :default-buttons="false">
+                <NuxtLink to="/" class="btn btn-primary join-item">Home</NuxtLink>
+            </HeroBanner>
+        </template>
+        <template v-else>
+            <HeroBanner :image="imageGeneric" :title="error.statusCode" subtitle="" :default-buttons="false">
+                <p class="mb-5 text-xl">
+                    <span class="italic">
+                        &quot;It's not a bug, it's a feature!&quot;
+                    </span>
+                    <br>
+                    An error has occurred. Please try again later.
+                </p>
+                <NuxtLink to="/" class="btn btn-primary join-item">Home</NuxtLink>
+            </HeroBanner>
+        </template>
+    </NuxtLayout>
+</template>
+  
