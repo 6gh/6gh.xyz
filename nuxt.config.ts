1f9f9c6b38fd8e7590d252110f939e2b3806a7b0
6gh <47262177+6gh@users.noreply.github.com>
2024-05-09 18:45:07 +0000

    fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-icon", '@nuxtjs/google-fonts'],
   extends: ["nuxt-umami"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ['Fira Code', 'sans-serif']
+          }
+        }
+      }
+    }
   },
   colorMode: {
     preference: "system", // default theme
     fallback: "main", // fallback theme
     dataValue: "theme", // activate data-theme in <html> tag
     classSuffix: "",
+  },
+  googleFonts: {
+    families: {
+      'Fira Code': true,
+      'Spline Sans Mono': true,
+    }
   },
   appConfig: {
     umami: {
