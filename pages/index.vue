74d8a76f61e4b45e014396d0bc22844598b9952c
6gh <47262177+6gh@users.noreply.github.com>
2024-05-09 00:35:09 -0400

    glitchy text
    - still debating to keep it

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,80 @@
 const bgPrimary = useCdn("/images/bg-primary.jpg");
 const currentPage = ref("0");
 
+const homeLinkText = ref("Home");
+const aboutLinkText = ref("About");
+const contactLinkText = ref("Contact");
+const midisLinkText = ref("MIDIs");
+const projectsLinkText = ref("Portfolio");
+
+function getRandomLetter() {
+    return String.fromCharCode(Math.floor(Math.random() * 52) + 65);
+}
+
+function onLinkHovered(pageNumber: string, event: MouseEvent) {
+    currentPage.value = pageNumber;
+
+    if (!event.target) {
+        return;
+    }
+
+    let text = (() => {
+        switch (pageNumber) {
+            case '0':
+                return homeLinkText;
+            case '1':
+                return aboutLinkText;
+            case '2':
+                return contactLinkText;
+            case '3':
+                return midisLinkText;
+            case '4':
+                return projectsLinkText;
+            default:
+                return homeLinkText;
+        }
+    })()
+
+    let elem = (event.target as HTMLElement);
+    let originalText = elem.dataset.text || (() => {
+        switch (pageNumber) {
+            case '0':
+                return homeLinkText.value;
+            case '1':
+                return aboutLinkText.value;
+            case '2':
+                return contactLinkText.value;
+            case '3':
+                return midisLinkText.value;
+            case '4':
+                return projectsLinkText.value;
+            default:
+                return homeLinkText.value;
+        }
+    })();
+
+    let it = 0;
+
+    const interval = setInterval(() => {
+        text.value = text.value.split('')
+            .map((_, i) => {
+                if (i < it) {
+                    return originalText[i];
+                } else {
+                    return getRandomLetter();
+                }
+            })
+            .join('');
+
+        if (it >= 10) {
+            clearInterval(interval);
+            text.value = originalText;
+        }
+
+        it = it + 1 / 2;
+    }, 30)
+}
+
 definePageMeta({
     layout: 'footer-only'
 })
@@ -11,39 +85,39 @@ definePageMeta({
     <div class="w-screen h-screen grid grid-cols-2">
         <div class="w-full h-full flex flex-col justify-center px-52">
             <ul class="w-fit h-fit text-6xl flex flex-col group nav-links" :group-hover:data-index="currentPage">
-                <li @mouseenter="currentPage = '0'" class="py-4">
+                <li data-text="Home" @mouseenter="onLinkHovered('0', $event)" class="py-4">
                     <NuxtLink
                         class="w-full h-full transition-all group-hover:opacity-60 group-hover:scale-90 group-hover:hover:opacity-100 group-hover:hover:scale-100"
                         to="/">
-                        Home
+                        {{ homeLinkText }}
                     </NuxtLink>
                 </li>
-                <li @mouseenter="currentPage = '1'" class="py-4">
+                <li data-text="About" @mouseenter="onLinkHovered('1', $event)" class="py-4">
                     <NuxtLink
                         class="w-full h-full transition-all group-hover:opacity-60 group-hover:scale-90 group-hover:hover:opacity-100 group-hover:hover:scale-100"
                         to="/about">
-                        About
+                        {{ aboutLinkText }}
                     </NuxtLink>
                 </li>
-                <li @mouseenter="currentPage = '2'" class="py-4">
+                <li data-text="Contact" @mouseenter="onLinkHovered('2', $event)" class="py-4">
                     <NuxtLink
                         class="w-full h-full transition-all group-hover:opacity-60 group-hover:scale-90 group-hover:hover:opacity-100 group-hover:hover:scale-100"
                         to="/contact">
-                        Contact
+                        {{ contactLinkText }}
                     </NuxtLink>
                 </li>
-                <li @mouseenter="currentPage = '3'" class="py-4">
+                <li data-text="MIDIs" @mouseenter="onLinkHovered('3', $event)" class="py-4">
                     <NuxtLink
                         class="w-full h-full transition-all group-hover:opacity-60 group-hover:scale-90 group-hover:hover:opacity-100 group-hover:hover:scale-100"
                         to="/midis">
-                        MIDIs
+                        {{ midisLinkText }}
                     </NuxtLink>
                 </li>
-                <li @mouseenter="currentPage = '4'" class="py-4">
+                <li data-text="Portfolio" @mouseenter="onLinkHovered('4', $event)" class="py-4">
                     <NuxtLink
                         class="w-full h-full transition-all group-hover:opacity-60 group-hover:scale-90 group-hover:hover:opacity-100 group-hover:hover:scale-100"
                         to="/projects">
-                        Portfolio
+                        {{ projectsLinkText }}
                     </NuxtLink>
                 </li>
                 <div :class="[`absolute top-0 left-0 w-screen h-screen -z-10 transition-all duration-700 ease-in-out
