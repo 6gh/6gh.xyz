c30712f4ce71fd78605031194df77e457f1db59f
6gh <47262177+6gh@users.noreply.github.com>
2024-05-10 09:39:19 -0400

    cross-fade images + cards on main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const appConfig = useAppConfig();
+
 const bgtemp1 = useCdn("/images/temp1.jpg");
 const bgtemp2 = useCdn("/images/temp2.jpg");
 const bgtemp3 = useCdn("/images/temp3.jpg");
@@ -6,13 +8,44 @@ const bgMidis = useCdn("/images/bg-primary.jpg");
 const bgtemp4 = useCdn("/images/temp4.jpg");
 
 const currentPage = ref("0");
-const currentImage = ref(bgtemp1)
 
 const homeLinkText = ref("Home");
 const aboutLinkText = ref("About");
 const contactLinkText = ref("Contact");
 const midisLinkText = ref("MIDIs");
 const projectsLinkText = ref("Portfolio");
+
+const cardTitle = ref("Welcome");
+const cardTitleInterval = ref<NodeJS.Timeout>();
+const cardText = ref("Welcome to 6gh's Website! Here you can find downloads to my MIDIs, contact information, what I'm working on, and more!");
+const cardTextInterval = ref<NodeJS.Timeout>();
+const cardTexts: {
+    [key: string]: {
+        title: string,
+        text: string
+    }
+} = {
+    "Home": {
+        title: "Welcome",
+        text: "Welcome to 6gh's Website! Here you can find downloads to my MIDIs, contact information, what I'm working on, and more!"
+    },
+    "About": {
+        title: "About",
+        text: "I'm a 16 year old developer who loves to make things. I'm currently working on a few projects, including a Discord bot and a website for a friend."
+    },
+    "Contact": {
+        title: "Contact",
+        text: "Find out how to contact me here! You can reach me through email, Discord, or through my social medias."
+    },
+    "MIDIs": {
+        title: "MIDIs",
+        text: "Download my MIDIs here! I've made a few MIDIs and you can find them all here available for download for free."
+    },
+    "Portfolio": {
+        title: "Portfolio",
+        text: "Check out what I'm working on! I'm currently working on a few projects"
+    },
+}
 
 function getRandomLetter() {
     return String.fromCharCode(Math.floor(Math.random() * 52) + 65);
@@ -28,19 +61,14 @@ function onLinkHovered(pageNumber: string, event: MouseEvent) {
     const text = (() => {
         switch (pageNumber) {
             case '0':
-                currentImage.value = bgtemp1
                 return homeLinkText;
             case '1':
-                currentImage.value = bgtemp2
                 return aboutLinkText;
             case '2':
-                currentImage.value = bgtemp3
                 return contactLinkText;
             case '3':
-                currentImage.value = bgMidis
                 return midisLinkText;
             case '4':
-                currentImage.value = bgtemp4
                 return projectsLinkText;
             default:
                 return homeLinkText;
@@ -85,6 +113,75 @@ function onLinkHovered(pageNumber: string, event: MouseEvent) {
 
         it = it + 1 / 2;
     }, 30)
+
+    if (cardTitleInterval.value) {
+        clearInterval(cardTitleInterval.value);
+    }
+
+    let titleIt = 0;
+
+    cardTitleInterval.value = setInterval(() => {
+        const { title } = cardTexts[originalText];
+        cardTitle.value = title.split('')
+            .map((_, i) => {
+                if (i < titleIt) {
+                    return title[i];
+                } else {
+                    return getRandomLetter();
+                }
+            })
+            .join('');
+
+        if (titleIt >= title.length) {
+            clearInterval(cardTitleInterval.value);
+            cardTitle.value = title;
+        }
+
+        titleIt = titleIt + 1 / 4;
+    }, 30);
+
+    if (cardTextInterval.value) {
+        clearInterval(cardTextInterval.value);
+    }
+
+    let textIt = 0;
+
+    cardTextInterval.value = setInterval(() => {
+        const { text } = cardTexts[originalText];
+        cardText.value = text.split('')
+            .map((char, i) => {
+                if (i < textIt) {
+                    return text[i];
+                } else {
+                    return getRandomLetter();
+                }
+            })
+            .join('');
+
+        if (textIt >= text.length) {
+            clearInterval(cardTextInterval.value);
+            cardText.value = text;
+        }
+
+        textIt = textIt + 5;
+    }, 30);
+}
+
+function getBackgroundPosition() {
+    switch (currentPage.value) {
+        case '0':
+            return 'bg-[position:center_40%]';
+        case '1':
+            return 'bg-[position:center_50%]';
+        case '2':
+            return 'bg-[position:center_60%]';
+        case '3':
+            return 'bg-[position:center_70%]';
+        case '4':
+            return 'bg-[position:center_80%]';
+        default:
+            return 'bg-[position:center_90%]';
+    }
 }
 
 definePageMeta({
@@ -95,29 +192,29 @@ definePageMeta({
 useHead({
     link: [
         {
+            rel: "preload",
+            as: "image",
             href: bgtemp1,
-            rel: "preload",
-            as: "image",
         },
         {
+            rel: "preload",
+            as: "image",
             href: bgtemp2,
-            rel: "preload",
-            as: "image",
         },
         {
+            rel: "preload",
+            as: "image",
             href: bgtemp3,
-            rel: "preload",
-            as: "image",
         },
         {
+            rel: "preload",
+            as: "image",
             href: bgMidis,
-            rel: "preload",
-            as: "image",
         },
         {
-            href: bgtemp4,
             rel: "preload",
             as: "image",
+            href: bgtemp4,
         },
     ],
 });
@@ -180,35 +277,40 @@ useHead({
                                 return 'bg-[position:0%_0%]';
                         }
                     })()]" />
-                <div :style="`background-image: url(${currentImage})`" :class="[`absolute top-0 left-0 w-screen h-screen -z-20 transition-all duration-700 ease-in-out
-                    bg-[size:120vmax]
-                    opacity-20 group-hover:opacity-15 group-hover:bg-[size:110vmax]`, (() => {
-                        switch (currentPage) {
-                            case '0':
-                                return 'bg-[position:center_40%]';
-                            case '1':
-                                return 'bg-[position:center_50%]';
-                            case '2':
-                                return 'bg-[position:center_60%]';
-                            case '3':
-                                return 'bg-[position:center_70%]';
-                            case '4':
-                                return 'bg-[position:center_80%]';
-                            default:
-                                return 'bg-[position:center_90%]';
-                        }
-                    })()]" />
+                <div class="-z-20 opacity-20 group-hover:opacity-15">
+                    <Transition>
+                        <div v-show="currentPage === '0'" :style="`background-image: url(${bgtemp1})`" :class="[`absolute top-0 left-0 w-screen h-screen transition-all duration-700 ease-in-out
+                        bg-[size:120vmax] group-hover:bg-[size:110vmax]`, getBackgroundPosition()]" />
+                    </Transition>
+                    <Transition>
+                        <div v-show="currentPage === '1'" :style="`background-image: url(${bgtemp2})`" :class="[`absolute top-0 left-0 w-screen h-screen transition-all duration-700 ease-in-out
+                        bg-[size:120vmax] group-hover:bg-[size:110vmax]`, getBackgroundPosition()]" />
+                    </Transition>
+                    <Transition>
+                        <div v-show="currentPage === '2'" :style="`background-image: url(${bgtemp3})`" :class="[`absolute top-0 left-0 w-screen h-screen transition-all duration-700 ease-in-out
+                        bg-[size:120vmax] group-hover:bg-[size:110vmax]`, getBackgroundPosition()]" />
+                    </Transition>
+                    <Transition>
+                        <div v-show="currentPage === '3'" :style="`background-image: url(${bgMidis})`" :class="[`absolute top-0 left-0 w-screen h-screen transition-all duration-700 ease-in-out
+                        bg-[size:120vmax] group-hover:bg-[size:110vmax]`, getBackgroundPosition()]" />
+                    </Transition>
+                    <Transition>
+                        <div v-show="currentPage === '4'" :style="`background-image: url(${bgtemp4})`" :class="[`absolute top-0 left-0 w-screen h-screen transition-all duration-700 ease-in-out
+                        bg-[size:120vmax] group-hover:bg-[size:110vmax]`, getBackgroundPosition()]" />
+                    </Transition>
+                </div>
             </ul>
         </div>
 
-        <div class="">
-            <TransitionGroup>
-                <div class="absolute" v-if="currentPage === '0'">home</div>
-                <div class="absolute" v-else-if="currentPage === '1'">About</div>
-                <div class="absolute" v-else-if="currentPage === '2'">MIDIs</div>
-                <div class="absolute" v-else-if="currentPage === '3'">Portfolio</div>
-                <div class="absolute" v-else-if="currentPage === '4'">Blog</div>
-            </TransitionGroup>
+        <div class="w-10/12 bg-base-300 my-auto flex px-6 py-8 text-primary-content">
+            <div class="flex flex-col gap-4 w-full">
+                <h1 class="text-4xl">
+                    {{ cardTitle }}
+                </h1>
+                <p class="break-words inline">
+                    {{ cardText }}
+                </p>
+            </div>
         </div>
     </div>
 </template>
@@ -217,11 +319,18 @@ useHead({
 /* we will explain what these classes do next! */
 .v-enter-active,
 .v-leave-active {
-    transition: opacity 0.5s ease;
+    transition: all 0.7s ease;
+    opacity: 1;
 }
 
-.v-enter-from,
+
+.v-enter-from {
+    transition: all 0.7s ease;
+    opacity: 0;
+}
+
 .v-leave-to {
+    transition: all 0.7s ease;
     opacity: 0;
 }
 </style>
